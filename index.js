52861e559089ade2adcde9f8b1e6be2482420df6
Denis Onder <denis_onder@protonmail.com>
2020-06-07 07:20:31 +0200

    Implemented LeagueOfGraphs instead of LolCounters

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const { Client, MessageEmbed } = require("discord.js");
-const fetch = require("node-fetch");
-const DOMParser = require("dom-parser");
+const { Client, MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+const DOMParser = require('dom-parser');
 
 const client = new Client();
-const KEYWORD = "!c";
+const KEYWORD = '!c';
 
 // Load .env
-require("dotenv").config();
+require('dotenv').config();
 
 const parser = new DOMParser();
 
@@ -14,20 +14,20 @@ const sanitize = (str) =>
   str
     .toLowerCase()
     .trim()
-    .split(" ")
-    .map((s) => s.trim().replace("'", ""));
+    .split(' ')
+    .map((s) => s.trim().replace("'", ''));
 
-const setActivity = (champName = "Shaco") => {
+const setActivity = (champName = 'name') => {
   client.user.setActivity(`LoL | !c ${champName}`, {
-    type: "PLAYING",
+    type: 'PLAYING',
   });
 };
 
 const generateMarkdown = (champArray) => {
-  let output = "```md\n";
+  let output = '```md\n';
 
   champArray.forEach((c, i) => (output += `${i + 1}. ${c.innerHTML}\n`));
-  output += "```";
+  output += '```';
 
   return output;
 };
@@ -43,22 +43,24 @@ function generateResponse(status, data = null) {
   return { status, data };
 }
 
+const parseTable = (table) => table.getElementsByClassName('name');
+
 function parseCountersFromHTML(html) {
   const document = parser.parseFromString(html);
-  const weakAgainstWrapper = document.getElementsByClassName("weak-block")[0];
-  const goodAgainstWrapper = document.getElementsByClassName("strong-block")[0];
 
-  const properChampName = weakAgainstWrapper
-    .getElementsByTagName("p")[0]
-    .innerHTML.split("is Weak Against")[0];
+  const [
+    goodAgainstTable,
+    _,
+    weakAgainstTable,
+  ] = document.getElementsByClassName('data_table');
 
-  const weakAgainst = weakAgainstWrapper
-    .getElementsByClassName("name")
-    .slice(0, 9);
+  console.log(goodAgainstTable, weakAgainstTable);
 
-  const goodAgainst = goodAgainstWrapper
-    .getElementsByClassName("name")
-    .slice(0, 9);
+  const properChampName = document.getElementsByTagName('h2')[0].innerHTML;
+
+  const weakAgainst = parseTable(weakAgainstTable);
+
+  const goodAgainst = parseTable(goodAgainstTable);
 
   return { champion: properChampName, counters: weakAgainst, goodAgainst };
 }
@@ -68,13 +70,13 @@ function parseBuildsFromHTML(html) {
 
   const document = parser.parseFromString(html);
   const builds = document
-    .getElementsByClassName("browse-list__item")
+    .getElementsByClassName('browse-list__item')
     .slice(0, 5);
 
   builds.forEach((b, i) =>
     output.push({
-      name: b.getElementsByTagName("h3")[0].innerHTML,
-      link: "https://www.mobafire.com" + builds[i].getAttribute("href"),
+      name: b.getElementsByTagName('h3')[0].innerHTML,
+      link: 'https://www.mobafire.com' + builds[i].getAttribute('href'),
     })
   );
 
@@ -85,18 +87,20 @@ function createEmbed(data) {
   const embed = new MessageEmbed();
 
   embed.setTitle(data.champion);
-  embed.addField("Weak Against", generateMarkdown(data.counters), true);
-  embed.addField("Good Against", generateMarkdown(data.goodAgainst), true);
-  embed.addField("Builds", generateBuildLinks(data.builds), false);
+  embed.addField('Weak Against', generateMarkdown(data.counters), true);
+  embed.addField('Good Against', generateMarkdown(data.goodAgainst), true);
+  embed.addField('Builds', generateBuildLinks(data.builds), false);
 
   return embed;
 }
 
 async function getCounterpicks(champion) {
-  const res = await fetch(`https://lolcounter.com/champions/${champion}`);
+  const res = await fetch(
+    `https://www.leagueofgraphs.com/champions/counters/${champion}`
+  );
 
   if (res.status !== 200)
-    return generateResponse(res.status, "Champion not found.");
+    return generateResponse(res.status, 'Champion not found.');
 
   const data = parseCountersFromHTML(await res.text());
 
@@ -104,14 +108,14 @@ async function getCounterpicks(champion) {
 }
 
 async function getBuilds(champion) {
-  champion = sanitize(champion).join("-");
+  champion = sanitize(champion).join('-');
 
   const res = await fetch(
     `https://www.mobafire.com/league-of-legends/${champion}-guide`
   );
 
   if (res.status !== 200)
-    return generateResponse(res.status, "No guides found.");
+    return generateResponse(res.status, 'No guides found.');
 
   const data = parseBuildsFromHTML(await res.text());
 
@@ -123,19 +127,19 @@ async function handleMessage(msg) {
 
   let [_, champName] = msg.cleanContent.split(KEYWORD);
 
-  if (!champName) return msg.reply("Invalid input.\n```!c <CHAMPION_NAME>```");
+  if (!champName) return msg.reply('Invalid input.\n```!c <CHAMPION_NAME>```');
 
-  champName = sanitize(champName).join("");
+  champName = sanitize(champName).join('');
 
   const { data: lolcounterData, status } = await getCounterpicks(champName);
 
-  if (status !== 200) return msg.reply("Invalid champion name.");
+  if (status !== 200) return msg.reply('Invalid champion name.');
 
   const { data: builds, status: bStatus } = await getBuilds(
     lolcounterData.champion
   );
 
-  if (bStatus !== 200) return msg.reply("No builds found.");
+  if (bStatus !== 200) return msg.reply('No builds found.');
 
   const data = { ...lolcounterData, builds };
 
@@ -144,18 +148,18 @@ async function handleMessage(msg) {
   msg.channel.send(createEmbed(data));
 }
 
-client.on("message", handleMessage);
+client.on('message', handleMessage);
 
-client.on("ready", () => {
+client.on('ready', () => {
   console.log(`Bot logged in.\n${client.user.tag}`);
   setActivity();
 });
 
 client
   .login(process.env.TOKEN)
-  .catch((err) => console.error("client.login", err));
+  .catch((err) => console.error('client.login', err));
 
-process.on("beforeExit", (code) => {
+process.on('beforeExit', (code) => {
   console.log(`Process will exit with code: ${code}`);
   process.exit(code);
 });
